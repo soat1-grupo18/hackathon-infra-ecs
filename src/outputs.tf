5ec0f9905953fd1d32c47fdc132251b166a60092
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2024-03-22 22:51:41 -0300

    chore: removing nlb
    - HTTP API Gateway does not need NLB;

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,6 +1,6 @@
-output "lb_vpclink_dns_name" {
-  value = aws_lb.lb_vpclink.dns_name
-}
+# output "lb_vpclink_dns_name" {
+#   value = aws_lb.lb_vpclink.dns_name
+# }
 
 output "lb_ingress_dns_name" {
   value = aws_lb.lb_ingress.dns_name
